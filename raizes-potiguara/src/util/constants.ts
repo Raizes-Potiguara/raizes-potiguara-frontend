84e305b8f6b320ac60c622b6d149a274669df9b0
emile.christie <emile.christie.pereira@ccc.ufcg.edu.br>
2026-07-05 10:37:15 -0300

    configurações de identidade visual

---
+++ raizes-potiguara/src/util/constants.ts
@@ -0,0 +1,26 @@
+export const CORES = {
+    BRANCO: "F8F2F2",
+    CINZA_CLARO: "AFAAA9",
+    CINZA_ESCURO: "635656",
+    PRETO: "2B2121",
+    VERMELHO_ESCURO: "671212",
+    VERMELHO_MEDIO: "892424",
+    VERMELHO_VIVO: "D21717",
+} as const;
+
+export const TAMANHO = {
+    TITULO_YBIRA: 64,
+    TITULO_PAGINA: 48,
+    SUBTITULO_PAGINA: 36,
+    TITULO_SECAO: 36,
+    SUBTITULO_SECAO: 24,
+    TITULO_SUBSECAO: 24,
+    SUBTITULO_SUBSECAO: 16,
+    TEXTO_BOTAO: 16,
+    CORPO_TEXTO: 16,
+    TEXTO_PEQUENO: 14,
+} as const;
+// tamanho padronizado de texto, pode ser alterado, mas altere diretamente aqui e não no componente!!
+
+export const RADIUS_PADRAO = 4;
+// corner/border radius padrão para componentes com pontas
